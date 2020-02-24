31339053e96f1b369e17e558c9745ca4fcbce616
Tatsuya Sato <satoryu.1981@gmail.com>
2020-02-24 16:04:54 +0900

    Implement  a skelton for cli

---
+++ lib/denso/calendar/cli.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Denso
+  module Calendar
+    class Cli
+      def self.run(options)
+        new(options).run
+      end
+
+      def initialize(options)
+      end
+
+      def run
+      end
+    end
+  end
+end
