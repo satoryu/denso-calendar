e4932471374c389aa41b5ffc52c3c9d0507a4602
Tatsuya Sato <satoryu.1981@gmail.com>
2020-07-31 15:37:29 +0900

    Bump up the supported ruby version to 2.6+
    2.5 or earlier version of ruby's super does not work as I expected.
    so I decided to omit 2.5 or earlier from supported ruby version.

--- lib/denso/calendar/date.rb
+++ lib/denso/calendar/date.rb
@@ -5,10 +5,11 @@ require 'date'
 module Denso
   class Calendar
     class Date < ::Date
-      def initialize(*args, holiday: true)
+      def initialize(*args)
+        options = args.last.is_a?(Hash) ? args.pop : { holiday: true }
         super(*args)
 
-        @holiday = holiday
+        @holiday = options[:holiday]
       end
 
       def holiday?
